14c1a483355c22ecbb9b6e40a116046ad3de56d4
Yuri Correa <yuricorrea@live.com>
2022-08-31 11:08:12 -0300

    change interpolate to interpolateNode

--- src/views/DrawerView.tsx
+++ src/views/DrawerView.tsx
@@ -6,7 +6,7 @@ import {
   ThemeContext,
   NavigationScreenProp,
 } from 'react-navigation';
-import { ScreenContainer } from 'react-native-screens';
+import { ScreenContainer, screensEnabled } from 'react-native-screens';
 
 import * as DrawerActions from '../routers/DrawerActions';
 import DrawerSidebar from './DrawerSidebar';
@@ -49,6 +49,7 @@ type Props = {
     contentOptions?: object;
   };
   screenProps: unknown;
+  detachInactiveScreens: boolean;
 };
 
 type State = {
@@ -60,6 +61,7 @@ type State = {
  * Component that renders the drawer.
  */
 export default class DrawerView extends React.PureComponent<Props, State> {
+  // eslint-disable-next-line react/sort-comp
   static contextType = ThemeContext;
   static defaultProps = {
     lazy: true,
@@ -159,17 +161,13 @@ export default class DrawerView extends React.PureComponent<Props, State> {
         drawerOpenProgress={progress}
         navigation={this.props.navigation}
         descriptors={this.props.descriptors}
-        contentComponent={this.props.navigationConfig.contentComponent}
-        contentOptions={this.props.navigationConfig.contentOptions}
-        drawerPosition={this.props.navigationConfig.drawerPosition}
-        style={this.props.navigationConfig.style}
         {...this.props.navigationConfig}
       />
     );
   };
 
   private renderContent = () => {
-    let { lazy, navigation } = this.props;
+    let { lazy, navigation, detachInactiveScreens = true } = this.props;
     let { loaded } = this.state;
     let { routes } = navigation.state;
 
@@ -185,8 +183,11 @@ export default class DrawerView extends React.PureComponent<Props, State> {
         />
       );
     } else {
+      const enabled = screensEnabled?.() && detachInactiveScreens;
+
       return (
-        <ScreenContainer style={styles.content}>
+        // @ts-ignore
+        <ScreenContainer enabled={enabled} style={styles.content}>
           {routes.map((route, index) => {
             if (lazy && !loaded.includes(index)) {
               // Don't render a screen if we've never navigated to it
@@ -204,6 +205,7 @@ export default class DrawerView extends React.PureComponent<Props, State> {
                   { opacity: isFocused ? 1 : 0 },
                 ]}
                 isVisible={isFocused}
+                enabled={detachInactiveScreens}
               >
                 <SceneView
                   navigation={descriptor.navigation}
